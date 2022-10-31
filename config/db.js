be72da9fdd27cb2fb1fafadd8f750721569a228b
Akash <akasharora375@gmail.com>
2022-10-30 20:42:58 -0400

    initial commit

---
+++ config/db.js
@@ -0,0 +1,5 @@
+// <!-- File: db.js Author: Manipal Singh Sidhu - Student Number: 300859319 WebApp Name: Midterm - Created on: October 27, 2021 -->
+
+module.exports = {
+    "AtlasDB": "mongodb+srv://akash:zEqiiE7s9dNcYzKB@cluster0.pcc10z1.mongodb.net/book_list?retryWrites=true&w=majority"
+}
